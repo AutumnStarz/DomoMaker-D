c48d5d6751dde647f8107777c596c8003e29ddcb
AutumnStarz <143517148+AutumnStarz@users.noreply.github.com>
2025-04-08 08:58:37 -0400

    commit

---
+++ server/router.js
@@ -0,0 +1,19 @@
+const controller = require('./controllers');
+const mid = require('./middleware');
+
+const router = (app) => {
+    app.get('/login', mid.requiresSecure, mid.requiresLogout, controller.Account.loginPage);
+    app.post('/login', mid.requiresSecure, mid.requiresLogout, controller.Account.login);
+
+    app.get('/signup', mid.requiresSecure, mid.requiresLogout, controller.Account.signupPage);
+    app.post('/signup', mid.requiresSecure, mid.requiresLogout, controller.Account.signup);
+
+    app.get('/logout', mid.requiresLogin, controller.Account.logout);
+    app.get('/maker', mid.requiresLogin, controller.Domo.makerPage);
+    app.post('/maker', mid.requiresLogin, controller.Domo.makeDomo);
+
+    app.get('/', mid.requiresSecure, mid.requiresLogout, controller.Account.loginPage);
+
+};
+
+module.exports = router;
